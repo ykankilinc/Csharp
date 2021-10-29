2c58655669519f8a4382ecdd0e10b78d21f56776
ykankilinc <ykankilinc@gmail.com>
2021-10-30 00:16:05 +0300

    Operatörler

---
+++ Operatorler/Operatorler/Program.cs
@@ -0,0 +1,17 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Information;
+using System;
+
+namespace Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İşlevlerine göre operatörler aşağıdaki gibi kategorilendirilir.");
+            Console.WriteLine("Atama ve İşlemli Atama Operatörleri =, +=, -=, *=, /=");
+            Console.WriteLine("İlişkisel Operatörler ==,!=, <, >, >=,<=");
+            Console.WriteLine("Aritmetik +, -, *, /, %, ++, --");
+
+        }
+    }
+}
