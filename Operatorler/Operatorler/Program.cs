99c88db2f9b2a4b4d9ec7725d2b5411990b0569b
ykankilinc <ykankilinc@gmail.com>
2021-10-30 01:15:05 +0300

    Operatörler

--- Operatorler/Operatorler/Program.cs
+++ Operatorler/Operatorler/Program.cs
@@ -8,9 +8,55 @@ namespace Operatorler
         static void Main(string[] args)
         {
             Console.WriteLine("İşlevlerine göre operatörler aşağıdaki gibi kategorilendirilir.");
+            Console.WriteLine("**********");
             Console.WriteLine("Atama ve İşlemli Atama Operatörleri =, +=, -=, *=, /=");
+            int int1 = 100;
+            Console.WriteLine(int1);
+            int int2 = 20;
+            Console.WriteLine(int2);
+            int1 -= 1;
+            Console.WriteLine(int1);
+            int2 += 1;
+            Console.WriteLine(int2);
             Console.WriteLine("İlişkisel Operatörler ==,!=, <, >, >=,<=");
+            
+            if (int1==int2)
+            {
+                Console.WriteLine("int1==int2");
+
+            };
+            if (int1<int2)
+            {
+                Console.WriteLine("int1 küçük int2 den ");
+
+            }
+            if(int1>int2)
+            {
+                Console.WriteLine("int1 büyük 1 den");
+            }
             Console.WriteLine("Aritmetik +, -, *, /, %, ++, --");
+            int sonuc = int1 + int2;
+            Console.WriteLine(sonuc);
+            sonuc =int1 - int2;
+            Console.WriteLine(sonuc);
+            sonuc = int1 / int2;
+            Console.WriteLine(sonuc);
+            sonuc = int1 * int2;
+            Console.WriteLine(sonuc);
+            sonuc = int1 % int2;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("Matıksal &&, ||, !");
+            bool isSuccess = true;
+            bool isCompleted = false;
+            if (isSuccess && isCompleted) 
+            Console.WriteLine("Perfect!");
+            if (isSuccess || isCompleted) 
+            Console.WriteLine("Great!");
+            if (isSuccess && !isCompleted) 
+            Console.WriteLine("Good!");
+
+
 
         }
     }
