be178eb66efab1382d2575b9c9afded7923762b0
ykankilinc <ykankilinc@gmail.com>
2021-10-31 21:23:03 +0300

    If-Else Yapısı ve Ternary If

---
+++ Debugging-Watch-Variables/Debugging-Watch-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Debugging_Watch_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Debugging, Watch ve Variables");
+
+        }
+    }
+}
---
+++ if-else-ternary/if-else-ternary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else_ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If-Else Yapısı ve Ternary If");
+            int time = DateTime.Now.Hour;
+            Console.WriteLine(time);
+            if (time >= 6 && time > 11)
+                Console.WriteLine("Günaydın");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Geceler");
+            string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time >= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
+
+        }
+    }
+}
