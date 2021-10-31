30d514125e037588b4bd5ddb1b49b4fd48f643ca
ykankilinc <ykankilinc@gmail.com>
2021-10-31 19:35:53 +0300

    Hata Yönetimi

---
+++ Try-Catch-Finally/Try-Catch-Finally/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Try_Catch_Finally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hata Yakalama");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
